5fb7c8b79545f947eced5ed9d5e9bacbeceeb36f
BuildTools <68638023@qq.com>
2015-09-25 00:33:41 +1000

    update finish

---
+++ GameView/Map.h
@@ -0,0 +1,33 @@
+// Map.h ... interface to Map data type
+
+#ifndef MAP_H
+#define MAP_H
+
+#include "Places.h"
+
+typedef struct edge{
+    LocationID  start;
+    LocationID  end;
+    TransportID type;
+} Edge;
+
+// graph representation is hidden 
+typedef struct MapRep *Map; 
+
+// operations on graphs 
+Map  newMap();  
+void disposeMap(Map g); 
+void showMap(Map g); 
+int  numV(Map g);
+int  numE(Map g, TransportID t);
+
+
+
+int outputConnections(Map g, LocationID start ,TransportID type , int *numLocations,LocationID loc[], int depth);
+void searchRail(Map g, LocationID curr,LocationID loc[], int *numLocations,int currentDepth,int depth);
+
+
+
+
+
+#endif
